6714def33f34480b287662e622ec613da4a7b955
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2022-03-10 08:30:40 -0300

    feat: application connected to database

---
+++ src/database/mongoose.database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectToDatabase = async () => {
+  await mongoose.connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@universitiesapicluster.jjedv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    (error) => {
+      if (error) {
+        return console.log(`Could not connect to MongoDB: ${error.message}`);
+      }
+
+      return console.log('Connected to MongoDB!');
+    },
+  );
+};
+
+module.exports = connectToDatabase;
